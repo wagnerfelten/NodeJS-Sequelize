e88f97c88daa1ca3773bd94cdbf73a2302416e63
WagnerRodrigues <felten19@gmail.com>
2023-03-16 11:00:48 -0300

    trabalhando com o express-handlebars para criacao de layout

--- src/server.js
+++ src/server.js
@@ -1,12 +1,12 @@
 const express = require("express");
-
 const app = express();
+const handlebars = require("express-handlebars");
 
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/index.html")
-});
+app.engine("handlebars", handlebars({defaultLayout: 'main'}));
+app.set("view engine", "handlebars");
+
+
 
 
 const PORT = 3333;
-
 app.listen(PORT, () => console.log(`SERVER RODANDO NA PORT ${PORT}`))
