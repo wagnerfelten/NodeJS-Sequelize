f860526a40723c16997ff71ae7082378e612af88
WagnerRodrigues <felten19@gmail.com>
2023-03-20 10:41:06 -0300

    Instaldo o body-parser para pegar dados da url atraves do metodos do post

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 const express = require("express");
 const app = express();
 const handlebars = require("express-handlebars");
+const bodyParser = require("body-parser");
+const { Sequelize } = require("sequelize");
 
 app.engine('handlebars', handlebars.engine({defaultLayout: "main"}));
 app.set("view engine", "handlebars");
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(express.static('public'));
 
-app.get("/criarPost", (req, res) => {
+const sequelize = new Sequelize('test', 'root', 'Wr@271019', {
+    host: "localhost",
+    dialect: 'mysql'
+});
+
+app.get("/criar", (req, res) => {
     res.render("form");
 });
 
+app.post("/add", (req, res) => {
+    res.send("Titulo: " + req.body.title + " Conteudo: " + req.body.content);
+})
 
 const PORT = 1919;
 app.listen(PORT, () => console.log(`SERVER RUNNIG https://localhost:${PORT}`));
